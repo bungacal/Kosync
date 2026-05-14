6faf996c01220dce94b32e55eb0fced878edbcf0
bungacal <bungcal@gmail.com>
2026-05-14 15:11:52 +0700

    feat: seeder kos dan kamar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kos;
+use App\Models\Room;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Room::query()->delete();
+        Kos::query()->delete();
+        User::query()->delete();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $owner = User::query()->create([
+            'name' => 'Sarah Pemilik Kos',
+            'email' => 'owner@kosync.test',
+            'password' => 'owner123',
+            'role' => 'owner',
+        ]);
+
+        $kos = Kos::query()->create([
+            'owner_id' => $owner->id,
+            'name' => 'Kos Kencana',
+        ]);
+
+        $owner->update(['kos_id' => $kos->id]);
+
+        $rooms = collect([
+            ['number' => '101', 'floor' => 'Lantai 1'],
+            ['number' => '102', 'floor' => 'Lantai 1'],
+            ['number' => '103', 'floor' => 'Lantai 1'],
+            ['number' => '104', 'floor' => 'Lantai 1'],
+            ['number' => '201', 'floor' => 'Lantai 2'],
+            ['number' => '202', 'floor' => 'Lantai 2'],
+            ['number' => '203', 'floor' => 'Lantai 2'],
+            ['number' => '204', 'floor' => 'Lantai 2'],
+        ])->map(fn (array $room) => Room::query()->create([
+            'kos_id' => $kos->id,
+            'number' => $room['number'],
+            'floor' => $room['floor'],
+            'status' => 'Kosong',
+        ]))->keyBy('number');
+
+        $tenant = User::query()->create([
+            'name' => 'Nama Penghuni Kos',
+            'email' => 'penghuni@kosync.test',
+            'password' => 'penghuni123',
+            'role' => 'tenant',
+            'kos_id' => $kos->id,
+            'room_id' => $rooms->get('101')->id,
+        ]);
+
+        $rooms->get('101')->update([
+            'tenant_id' => $tenant->id,
+            'status' => 'Terisi',
         ]);
     }
 }
